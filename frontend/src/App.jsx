4ecf429a01d6546c590909e4eb22fa1dfd6dadec
Mohammad Hammad <hammad.scripted@gmail.com>
2026-07-01 00:31:14 +0530

    sidebar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,51 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/auth/login/LoginPage';
 import SignUpPage from './pages/auth/signup/SignUpPage';
+import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
+import Sidebar from './components/common/Sidebar';
+import RightPanel from './components/common/RightPanel';
+
+const AppLayout = ({ children }) => {
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			<Sidebar />
+			{children}
+			<RightPanel />
+		</div>
+	);
+};
 
 function App() {
 	return (
-		<div className='flex max-w-6xl mx-auto'>
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/signup' element={<SignUpPage />} />
-				<Route path='/login' element={<LoginPage />} />
-			</Routes>
-		</div>
+		<Routes>
+			<Route
+				path='/'
+				element={
+					<AppLayout>
+						<HomePage />
+					</AppLayout>
+				}
+			/>
+			<Route
+				path='/notifications'
+				element={
+					<AppLayout>
+						<NotificationPage />
+					</AppLayout>
+				}
+			/>
+			<Route
+				path='/profile/:username'
+				element={
+					<AppLayout>
+						<ProfilePage />
+					</AppLayout>
+				}
+			/>
+			<Route path='/signup' element={<SignUpPage />} />
+			<Route path='/login' element={<LoginPage />} />
+		</Routes>
 	);
 }
 
